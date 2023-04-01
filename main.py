c0a208af71a6ada8f42ed5aa81aa8404a9d862ba
habinax <47693655+habinax@users.noreply.github.com>
2023-04-01 14:19:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 import cv2
 import pandas as pd
 import configparser
+import time
 
 import gazedata_preprocession
 from GazeBehaviourProcession import GazeBehaviourProcession
@@ -27,21 +28,38 @@ kernelsigma = float(config['SETTINGS']['KernelSigma'])
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     if config['SETTINGS']['DataPreProcession'] == "True":
         print("Gazedata Vorverarbeitung läuft...")
         gazedata_preprocession.pipeline()
         print("Gazedata Vorverarbeitung abgeschlossen.")
+    end_time = time.time()
+    execution_time = end_time - start_time
+    with open('execution_time_gazedata_preprocession.txt', 'a') as file:
+        file.write(f'{gazedata_preprocession}: {execution_time:.6f} seconds\n')
 
+    start_time = time.time()
     if config['SETTINGS']['SplitVideo'] == "True":
         print("Video wird in Frames aufgeteilt...")
         video_procession.splitVideosToFrames()
         print("Aufteilung in Frames abgeschlossen.")
-
+    end_time = time.time()
+    execution_time = end_time - start_time
+    with open('execution_time_video_splitting.txt', 'a') as file:
+        file.write(f'{video_splitting}: {execution_time:.6f} seconds\n')
+    
+    start_time = time.time()
     if config['SETTINGS']['CreateIttiKochFrames'] == "True":
         print("Saliency Maps werden erstellt...")
         ittiKochFrameProcession.ittiKochFrameProcession()
         print("Erstellen der Saliency Maps abgeschlossen.")
-
+    end_time = time.time()
+    execution_time = end_time - start_time
+    with open('execution_time_saliency_maps.txt', 'a') as file:
+        file.write(f'{saliency_maps}: {execution_time:.6f} seconds\n')
+    
+    start_time = time.time() 
+    
     filesToProcess = [f for f in os.listdir(matchedDataPath) if isfile(join(matchedDataPath, f))]
     keyList = [f.split(".")[0].split("_")[-1] for f in filesToProcess]
     keyList = list(dict.fromkeys(keyList))
@@ -74,3 +92,9 @@ if __name__ == '__main__':
         fpscount = videofpscount[videonames.index(matchedDataName.split("_")[-1].split(".")[0])]
         length = len(df.index)/(eyeTrackerSamplingRate / fpscount)
         proc.matchGazeDataToFrame(finalFrameList[keyList.index(item.split(".")[0].split("_")[-1])][1],0,int(length))
+    
+    end_time = time.time()
+    execution_time = end_time - start_time
+    with open('execution_time_10_result_files.txt', 'a') as file:
+        file.write(f'{result_files}: {execution_time:.6f} seconds\n')
+    

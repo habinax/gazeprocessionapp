7332b3c7dcce0e0f9b44a74e9b139de702ea5018
habinax <47693655+habinax@users.noreply.github.com>
2023-04-02 17:35:37 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,7 @@
-py -m virtualenv venv &  if not exist "venv\data" mkdir venv\data & if not exist "venv\data\videos" mkdir venv\data\videos & if not exist "venv\data\gazedata" mkdir venv\data\gazedata & call venv\Scripts\activate && py -m pip install -r requirements.txt
+py -m virtualenv venv 
+if not exist "venv\data" mkdir venv\data 
+if not exist "venv\data\videos" mkdir venv\data\videos
+if not exist "venv\data\gazedata" mkdir venv\data\gazedata
+call venv\Scripts\activate && py -m pip install -r requirements.txt
 echo Import der Libraries abgeschlossen.
 pause
